bbdaea52c500af5fff74c1100faf21a2e5b0b794
eblo-max <pastornapas93q@gmail.com>
2025-04-04 15:37:53 +0400

    Delete services directory

--- services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Основной модуль бота-детектива
-"""
-
-from .claude_service.claude_service import ClaudeService
-from .news_service import NewsService
-from .profile_service import ProfileService
-
-__all__ = ["ClaudeService", "NewsService", "ProfileService"]
--- services/claude_service/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Модуль для работы с Claude API."""
-
-from .cache import AsyncTTLCache
-from .templates import PROMPT_TEMPLATES, get_system_prompt
-from .claude_service import ClaudeService
-
-__all__ = ["AsyncTTLCache", "PROMPT_TEMPLATES", "get_system_prompt", "ClaudeService"]
