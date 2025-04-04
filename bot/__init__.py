54975820e31d50c6a24a566778bde2894f13ba39
eblo-max <pastornapas93q@gmail.com>
2025-04-04 15:37:25 +0400

    Delete bot directory

--- bot/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Основной пакет бота
-"""
-
-from bot.core.bot import DetectiveBot
-from bot.core.config import load_config, BotConfig
-
-__all__ = ["DetectiveBot", "load_config", "BotConfig"]
--- bot/database/models/resources.py
+++
@@ -1,5 +0,0 @@
-from .energy import Energy
-from .inventory import Inventory
-from .reputation import Reputation
-
-__all__ = ["Energy", "Inventory", "Reputation"]
